21523b55aa2ce77a7dcacc320d72dcbd5b98a1b9
Jin <mina60832@gmail.com>
2024-06-13 13:29:56 +0800

    0613.2

--- vite.config.mjs
+++ vite.config.mjs
@@ -48,12 +48,12 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/android-chrome-192x192.png',
+            src: './android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/android-chrome-512x512.png',
+            src: './android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
@@ -62,7 +62,7 @@ export default defineConfig({
     }),
     VitePluginRadar({
       analytics: {
-        id: 'G-ZK7R14WLZ9'
+        id: 'G-EP7N8FWQZ9'
       }
     }),
     VueRouter(),
